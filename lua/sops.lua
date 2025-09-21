264f26c09e05cb2d40dcba030e488ed03736b9ce
nobody <hugosum.dev@protonmail.com>
2025-09-21 09:21:09 +0100

    progress

--- lua/sops.lua
+++ lua/sops.lua
@@ -1,5 +1,5 @@
 local util = require("util")
-local lyaml   = require "lyaml"
+local lyaml = require("lyaml")
 
 ---@class SopsModule
 local M = {}
@@ -119,24 +119,28 @@ M.setup = function(opts)
     "FileReadPost",
   }, {
     group = main_au_group,
-    callback = function()
-        local cwd = vim.fn.getcwd()
-        local root_patterns = {".sops.yaml"}
-        local matches = vim.fs.find(root_patterns, { upward = true, stop = cwd, type = "file", follow = true })
+    callback = function(ev)
+      if not util.is_sops_encrypted(ev.buf) then
+        return
+      end
 
-        if #matches == 0 then
-            return
-        end
+      local cwd = vim.fn.getcwd()
+      local root_patterns = { ".sops.yaml" }
+      local matches = vim.fs.find(root_patterns, { upward = true, stop = cwd, type = "file", follow = true })
 
-        -- TODO handle more than 1 match
-        local sops_config_path = matches[1]
-        -- local sops_config_dir = vim.fs.dirname(sops_config_path)
-        local sops_config_text = table.concat(vim.fn.readfile(sops_config_path), "\n")
-        local sops_config = lyaml.load(sops_config_text)
+      if #matches == 0 then
+        return
+      end
 
-        for i, rule in ipairs(sops_config.creation_rules) do
-            print(i, vim.inspect(rule))
-        end
+      -- TODO handle more than 1 match
+      local sops_config_path = matches[1]
+      -- local sops_config_dir = vim.fs.dirname(sops_config_path)
+      local sops_config_text = table.concat(vim.fn.readfile(sops_config_path), "\n")
+      local sops_config = lyaml.load(sops_config_text)
+
+      -- for i, rule in ipairs(sops_config.creation_rules) do
+      --     print(i, vim.inspect(rule))
+      -- end
     end,
   })
 end
--- lua/util.lua
+++ lua/util.lua
@@ -1,25 +1,15 @@
 local M = {}
 
-local SOPS_MARKER_BYTES = {
-  ["yaml"] = "mac: ENC[",
-  ["yaml.helm-values"] = "mac: ENC[",
-  ["json"] = '"mac": "ENC[',
-}
+local SOPS_MARKER_BYTES = "ENC["
 
+-- Use a better validation function, once sops has implemented validation https://github.com/getsops/sops/issues/437
+---@param bufnr number
+---@return boolean
 M.is_sops_encrypted = function(bufnr)
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-
-  local marker = SOPS_MARKER_BYTES[filetype]
-  if not marker then
-    return false
-  end
-
-  for _, line in ipairs(lines) do
-    if string.find(line, marker, nil, true) then
-      return true
-    end
-  end
+  local content = vim.fn.join(lines, "\n")
+  -- local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+  return string.find(content, SOPS_MARKER_BYTES, 1, true) ~= nil
 end
 
 return M
